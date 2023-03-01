9df54fd163fb619b6eaece409ec0a26b4adcc379
Anrishainidze <123399273+Anrishainidze@users.noreply.github.com>
2023-03-01 22:54:11 +0400

    ssss

---
+++ script.js
@@ -0,0 +1,47 @@
+////////1/////////
+// function User(model,make) {
+//     this.model = model;
+//     const _make = make;
+//     this.GetCarInfo = function () {
+//         document.write("model" + this.model + "make" + _make);
+//     };
+//     this.getMake = function (){
+//         return _make;
+//     };
+//     this.setMake = function (make){
+//         if(typeof make === "number" && make > 0 && make <200){
+//             return (_make = make);
+//         } else {
+//             alert("none info");
+//         }
+//     };
+// }
+// const henry = new User("Tom", 24)
+// document.write(henry._age)
+// document.write(henry.getAge())
+// henry.setAge(31)
+
+
+
+////2////
+// function Person(name, age, ) {
+//     this.name = name;
+//     const _age = age;
+//     this.displayInfo = function () {
+//         document.write("name" + this.name + "age" + _age);
+//     };
+//     this.getMake = function (){
+//         return _name;
+//     };
+//     this.setMake = function (name){
+//         if(typeof make === "number" && make > 0 && make <200){
+//             return (_name = name);
+//         } else {
+//             alert("none info");
+//         }
+//     };
+// }
+// const any = new User("Max", 22)
+// document.write(any._age)
+// document.write(any.getAge())
+// any.setAge(43)
